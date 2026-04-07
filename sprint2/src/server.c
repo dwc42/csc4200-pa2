1e58c158be742111b6c6b300f53a165c33aaf104
mrpatches123 <190913771+dwc42@users.noreply.github.com>
2026-04-07 15:14:01 -0500

    fixed

--- sprint2/src/server.c
+++ sprint2/src/server.c
@@ -49,7 +49,7 @@ void onConnectionCallback(int server_socket, ServerConfig serverConfig, Connecti
 					return;
 				}
 				sprintf(fileName, "%s", filePacket.payload + fileNameTagLength);
-				printf("FILENAME: %s", fileName);
+				printf("FILENAME: %s\n", fileName);
 				char *correctPayload = strchr(filePacket.payload, '\0') + 1;
 				FILE *filePtr = fopen(fileName, "a");
 				if (filePtr == NULL)
